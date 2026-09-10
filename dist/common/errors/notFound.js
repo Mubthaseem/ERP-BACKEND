edd552ef2af8a012b87a46d6d0f7b8f76806d9f3
Mubthaseem <mubthaseembinalavi@gmail.com>
2026-09-10 13:14:42 +0530

    feat: clean backend source code for ERP-BACKEND

--- dist/common/errors/notFound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = void 0;
-const ApiError_js_1 = require("./ApiError.js");
-const notFound = (req, res, next) => {
-    next(ApiError_js_1.ApiError.notFound(`Route not found: ${req.method} ${req.originalUrl}`));
-};
-exports.notFound = notFound;
